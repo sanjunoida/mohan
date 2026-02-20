dfc464d653b457f5a08ce89ed0e56fcce28a9394
sanjay singh <sanjaysingh@sanjays-MacBook-Air.local>
2026-02-20 17:17:47 +0530

    feature line added

---
+++ main.tf
@@ -0,0 +1,130 @@
+resource "azurerm_resource_group" "rg" {
+  name     = var.rg_name
+  location = var.rg_location
+}
+
+//dddd
+// added featu
+//ererr
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.vnet_addressspace
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnetname
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.subnetaddressspace
+}
+
+resource "azurerm_public_ip" "pip" {
+
+  name                = "sanjaypublicip3"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku                 = "Standard"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "sanjaynic3"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
+
+
+resource "azurerm_network_security_group" "nsg" {
+  name                = "sanjay-nsg"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "Allow-SSH"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
+data "azurerm_client_config" "current" {
+}
+
+
+resource "azurerm_key_vault" "kv" {
+  
+    name                        = "sanjaykv3"
+    location                    = azurerm_resource_group.rg.location
+    resource_group_name         = azurerm_resource_group.rg.name
+    tenant_id                   = data.azurerm_client_config.current.tenant_id
+    sku_name                    = "standard"
+
+    access_policy {
+        tenant_id = data.azurerm_client_config.current.tenant_id
+        object_id = data.azurerm_client_config.current.object_id
+    
+        secret_permissions = [
+            "Get",
+            "List",
+            "Set",
+            "Delete"
+        ]
+    }
+   
+}
+
+resource "azurerm_key_vault_secret" "password" {
+  name         = "pwd-secret"
+  value       = "Password@1234"
+  key_vault_id = azurerm_key_vault.kv.id
+}
+
+
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "sanjay-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_D2s_v3"
+  admin_username      = "azureuser"
+
+  network_interface_ids = [
+    azurerm_network_interface.nic.id,
+  ]
+
+  admin_password = azurerm_key_vault_secret.password.value
+  disable_password_authentication = false
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
